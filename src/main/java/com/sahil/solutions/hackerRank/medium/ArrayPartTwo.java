8252c743f3e3ac408a21bfcacc02954e9045ede2
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-05 12:10:17 +0000

    Solve ArrayPartTwo, update path

--- src/main/java/com/sahil/solutions/hackerRank/medium/ArrayPartTwo.java
+++ src/main/java/com/sahil/solutions/hackerRank/medium/ArrayPartTwo.java
@@ -1,26 +1,25 @@
-package com.sahil.solutions.hackerRank;
+package com.sahil.solutions.hackerRank.medium;
 
 import java.util.Scanner;
 
-public class JavaArrayPartTwo {
+public class ArrayPartTwo {
     public static boolean canWin(int leap, int[] game) {
         return validate(0, leap, game);
     }
 
     public static boolean validate(int pos, int leap, int[] game) {
-        if (pos >= game.length)
-            return true;
-        if (pos < 0 || game[pos] == 1)
-            return false;
-        game[pos] = 1;
-        return validate(pos - 1, leap, game)
-                || validate(pos + 1, leap, game)
-                || validate(pos + leap, leap, game);
+        if (pos >= game.length) return true; // >= since pos starts from 0.
+        if (pos < 0 || game[pos] == 1) return false;
+        game[pos] = 1; // Visited, hence setting to 1.
+        return
+        validate(pos - 1, leap, game)
+        || validate(pos + 1, leap, game)
+        || validate(pos + leap, leap, game);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int queries = scanner.nextInt();
+        int queries = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < queries; i++) {
             int size = scanner.nextInt();
             int leap = scanner.nextInt();
@@ -36,4 +35,5 @@ public class JavaArrayPartTwo {
         }
         scanner.close();
     }
+    
 }
