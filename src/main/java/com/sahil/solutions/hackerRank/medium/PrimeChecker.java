6519a321fb0b262e857d95c8c75461f27881f913
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-05 21:58:36 +0000

    Solve PrimeChecker from HR

---
+++ src/main/java/com/sahil/solutions/hackerRank/medium/PrimeChecker.java
@@ -0,0 +1,39 @@
+package com.sahil.solutions.hackerRank.medium;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static class Prime {
+        public void checkPrime(int... nums) {
+            for (int n : nums) {
+                if (isPrime(n)) {
+                    System.out.print(n + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        public boolean isPrime(int n) {
+            if (n <= 1) return false; // Primes start from 2.
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                // Not a prime if divisible by any num between 2 to √n (inclusive).
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+        int n4 = Integer.parseInt(scanner.nextLine());
+        int n5 = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        Prime prime = new Prime();
+        prime.checkPrime(n1);
+        prime.checkPrime(n1, n2);
+        prime.checkPrime(n1, n2, n3);
+        prime.checkPrime(n1, n2, n3, n4, n5);
+    }    
+}
